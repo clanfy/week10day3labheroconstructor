1316c871606937907924f185c143436faa8b5431
Clacson <cookiellacson@gmail.com>
2016-11-30 17:59:06 +0000

    food constructors completed

---
+++ food.js
@@ -0,0 +1,12 @@
+var Food = function(name, replenishmentValue, poisonous){
+  this.name = name;
+  this.replenishmentValue = replenishmentValue;
+  this.poisonous = poisonous;
+};
+
+var shrooms = new Food("shrooms", 10, false);
+var chocolateFrogs = new Food("chocolate frogs", 15, false);
+var donuts = new Food("donuts", 5, false);
+var kale = new Food("kale", 20, false);
+
+module.exports = [shrooms, chocolateFrogs, donuts,kale];
--- hero.js
+++ hero.js
@@ -2,13 +2,13 @@ var Hero = function(name, health, faveFood){
   this.name = name;
   this.health = health;
   this.faveFood = faveFood;
-}
+};
 
 Hero.prototype = {
   talk: function(){
     return "Good day, I am " + this.name + " and I am here to do my thing."
-  }
-}
+  };
+};
 
 var drStrange = new Hero("Stephen Strange", 100, "shrooms");
 var harry = new Hero("Harry Potter", 50, "chocolate frogs");
